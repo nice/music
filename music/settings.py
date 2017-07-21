97e146442e26f971f2faa0177388976b52dd6c05
Jayaram R <rush2jrp@gmail.com>
2017-07-21 11:50:33 +0530

    Completed settings for heroku deployment

--- music/settings.py
+++ music/settings.py
@@ -112,3 +112,15 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_DIR, 'static'),
 )
+
+# Heroku Override
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS as TCP
+# DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
